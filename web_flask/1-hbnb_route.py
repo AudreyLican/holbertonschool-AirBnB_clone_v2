4c79e842862348979d058320565966b707aa1bb9
AudreyLican <audrey.lican@gmail.com>
2024-04-10 23:18:16 +0200

    code style fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,17 +5,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-#route to display Hello HBNB!
+# route to display Hello HBNB!
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    #display HBNB
+    # display HBNB
     return "HBNB"
 
 
 if __name__ == '__main__':
-    #run the flask app
+    # run the flask app
     app.run(host='0.0.0.0', port=5000)
