0ca01b39d353d5ce7f548f0c8933d026c6042a71
AudreyLican <audrey.lican@gmail.com>
2024-04-13 00:03:19 +0200

    create task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def city_by_states():
+    # display HTML page with a list of all state obj in Storage
+    states = storage.all("State")
+    return render_template("8-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
